04a4a69b741131b5f47dbb64f952991b72879c77
aicha-lrt <92796149+aicha-lrt@users.noreply.github.com>
2023-03-31 13:15:42 +0200

    Update BaseDeDonneesTests.java

--- src/test/java/com/example/demo/data/BaseDeDonneesTests.java
+++ src/test/java/com/example/demo/data/BaseDeDonneesTests.java
@@ -13,6 +13,16 @@ public class BaseDeDonneesTests {
 
     @Test
     void uneVoiture(){
+
+        Voiture voiture = new Voiture();
+
+        voiture.setMarque("Mercedes");
+        voiture.setPrix(35000);
+
+        Assert.notNull(voiture,"La voiture ne peut pas être NULL");
+        Assert.notNull(voiture.getMarque(),"La marque ne doit pas être NULL");
+        Assert.isTrue(voiture.getPrix()>0,"Le prix doit être supérieur à 0");
+
     }
 
 }
