adfe27130ecf5fed1812705affe44785f3b3e38a
aicha-lrt <92796149+aicha-lrt@users.noreply.github.com>
2023-03-31 13:14:17 +0200

    Update Voiture.java

--- src/main/java/com/example/demo/data/Voiture.java
+++ src/main/java/com/example/demo/data/Voiture.java
@@ -4,3 +4,41 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+@Entity
+public class Voiture {
+
+    int id;
+    String marque;
+    double prix;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+
+
+}
