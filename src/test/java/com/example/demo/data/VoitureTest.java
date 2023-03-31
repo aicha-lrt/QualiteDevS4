15fc2e382e55c32e9bdef484f12c69c7314f7ab4
aicha-lrt <92796149+aicha-lrt@users.noreply.github.com>
2023-03-31 13:22:01 +0200

    Update VoitureTest.java

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,6 +9,16 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
+        
+        Voiture voiture = new Voiture();
+
+        voiture.setMarque("Mercedes");
+        voiture.setPrix(35000);
+
+        Assert.notNull(voiture,"La voiture ne peut pas être NULL");
+        Assert.notNull(voiture.getMarque(),"La marque ne doit pas être NULL");
+        Assert.isTrue(voiture.getPrix()>0,"Le prix doit être supérieur à 0");
+        
     }
 
 }
